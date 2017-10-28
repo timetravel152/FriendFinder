343b589e43a0670dc06ce8d893fc4738b13e7700
Lisa <timetravel152@gmail.com>
2017-10-27 21:58:55 -0500

    why not everything at once
    uploading it all at once

---
+++ app/data/friends.js
@@ -0,0 +1,15 @@
+var friends = [ {
+    "name" : "Monica",
+    "photo" : "https://media.giphy.com/media/iRbcE0NtzMJsQ/giphy.gif",
+    "scores" : [5,5,2,3,1,5,2,1,3,5]
+}, {
+    "name" : "Rachel",
+    "photo" : "https://media.giphy.com/media/jy0w1Jyg8tq8g/giphy.gif",
+    "scores" : [1,5,1,1,1,1,1,1,5,5]
+}, {
+    "name" : "Joey",
+    "photo" : "http://chaichalk.com/wp-content/uploads/2015/10/tumblr_mnd1onyb671rmifvmo1_500-1.gif",
+    "scores" : [5,3,2,3,5,5,1,3,3,4]
+}];
+
+module.exports = friends;
